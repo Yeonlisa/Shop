2f6b72c1b9ad9e783be812bb0f4e2d2c8532e093
yeonlisa <tkzw4869@gmail.com>
2021-12-31 14:04:41 +0900

    숙제 해설 : 상품목록 Component화 + 반복문

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
@@ -42,26 +43,26 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-            <h4>{ shoes[0].title }</h4>
-            <p>{ shoes[0].content} & { shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
-            <h4>{ shoes[1].title }</h4>
-            <p>{ shoes[1].content} & { shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
-            <h4>{ shoes[2].title }</h4>
-            <p>{ shoes[2].content} & { shoes[2].price}</p>
-          </div>
+          {
+            shoes.map((a,i) => {
+              return <Card shoes={shoes[i]} i={i} key={i}/>
+            })
+          }
         </div>
       </div>
 
     </div>
   );
+}
+
+function Card(props) {
+  return (
+      <div className="col-md-4">
+        <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"/>
+        <h4>{ props.shoes.title }</h4>
+        <p>{ props.shoes.content} & { props.shoes.price}</p>
+      </div>
+  )
 }
 
 export default App;
