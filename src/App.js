3c352a0473f4eb90c3c52c717020bee557d185c1
yeonlisa <tkzw4869@gmail.com>
2022-01-02 16:24:37 +0900

    React Router 2 : Link, Switch, history 기능

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import Detail from './Detail.js';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -18,8 +19,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -32,49 +33,45 @@ function App() {
         </Container>
       </Navbar>
 
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className='background'>
+            <h1>20% Season Off!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a,i) => {
+                  return <Card shoes={shoes[i]} i={i} key={i}/>
+                })
+              }
+            </div>
+          </div>
+        </Route>
+        
+        <Route path="/detail">
+          <Detail />
+        </Route>
 
-      <Route exact path="/">
-        <Jumbotron className='background'>
-          <h1>20% Season Off!</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((a,i) => {
-                return <Card shoes={shoes[i]} i={i} key={i}/>
-              })
-            }
-          </div>
-        </div>
-      </Route>
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
-          </div>
-        </div> 
-      </Route>
+        <Route path="/:id">
+          <div>아무거나적었을때 이거 보여주셈</div>
+        </Route>
+      </Switch>
+
       {/* <Route path="/어쩌구" component={Modal}></Route> */}
 
 
     </div>
   );
 }
+
 
 function Card(props) {
   return (
