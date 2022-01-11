1d60c4e02c49dd6bd02495a768099ff68931e78b
yeonlisa <tkzw4869@gmail.com>
2022-01-11 16:33:43 +0900

    Tab 만들기와 리액트에서의 애니메이션 (react-transition-group)

--- src/Detail.js
+++ src/Detail.js
@@ -4,6 +4,9 @@ import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
 import { 재고context } from './App.js';
+import { CSSTransition } from 'react-transition-group';
+import { Nav } from 'react-bootstrap';
+
 
 const 박스 = styled.div`
     padding: 20px;
@@ -19,6 +22,11 @@ function Detail(props) {
     
     let [alert, alert변경] = useState(true);
     let [inputData, inputData변경] = useState('');
+
+    let [누른탭, 누른탭변경] = useState(0);
+    let [스위치, 스위치변경] = useState(false);
+
+
     let 재고 = useContext(재고context);
 
     useEffect(() => {
@@ -68,9 +76,40 @@ function Detail(props) {
                     }}>뒤로가기</button> 
                 </div>
             </div>
+
+            <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link-0" onClick={ () => { 스위치변경(false); 누른탭변경(0) }}>Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1" onClick={ () => { 스위치변경(false); 누른탭변경(1) }}>Option 2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <CSSTransition in={스위치} classNames="wow" timeout={500}>
+                <TabContent 누른탭={누른탭} 스위치변경={스위치변경}/>
+            </CSSTransition>
+
+
         </div>
     )
 }
+
+function TabContent(props) {
+
+    useEffect(() => {
+        props.스위치변경(true);
+    });
+
+    if(props.누른탭 === 0) {
+        return <div>0번째 내용입니다</div>
+    } else if(props.누른탭 === 1) {
+        return <div>1번째 내용입니다</div>
+    } else if(props.누른탭 === 2) {
+        return <div>2번째 내용입니다</div>
+    }
+
+} 
 
 function Info(props) {
     return (
