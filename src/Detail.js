771e159f8ed6c4e3c727ac14a236a1d4ece09e0d
yeonlisa <tkzw4869@gmail.com>
2022-01-16 13:31:56 +0900

    세계최고로 쉬운 Redux 4 : dispatch할 때 데이터 실어보낼 수 있음

--- src/Detail.js
+++ src/Detail.js
@@ -6,6 +6,7 @@ import './Detail.scss';
 import { 재고context } from './App.js';
 import { CSSTransition } from 'react-transition-group';
 import { Nav } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 
 const 박스 = styled.div`
@@ -70,6 +71,8 @@ function Detail(props) {
 
                     <button className="btn btn-danger" onClick={() => {
                         props.재고변경([9,11,12])
+                        props.dispatch({ type: '항목추가', payload : { id : 2, name : '새로운상품', quan : 1 } });
+                        history.push('/cart');
                     }}>주문하기</button>&nbsp;
                     <button className="btn btn-danger" onClick={() => { 
                         history.push('/')
@@ -117,4 +120,14 @@ function Info(props) {
     )
 }
 
-export default Detail;
+function state를props화(state) {
+    return {
+        state : state.reducer,
+        alert열렸니 : state.reducer2
+    }
+}
+
+
+export default connect(state를props화)(Detail)
+
+/* export default Detail; */
